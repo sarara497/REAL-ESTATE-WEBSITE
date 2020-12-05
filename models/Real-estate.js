f0b7873cb97baa2e389a40bbe0e3fb3dd831adb0
Sara Al-Ayed <sara19972020@outlook.com>
2020-12-05 15:18:34 +0200

    create the user and real-estate schema

--- models/Real-estate.js
+++ models/Real-estate.js
@@ -2,10 +2,73 @@ const mongoose = require('mongoose')
 
 
 //create the schema 
-
+const Real_Estate = mongoose.Schema({
+    location :{
+        type: String,
+        required :true
+      },
+    real_type :{
+        type: String,
+        required :true
+      },
+    area: {
+          type: String,
+          required: true,
+      },
+    price: {
+        type: String,
+        required: true,
+      },
+    lowest_price: {
+        type: String,
+        required: true,
+    },
+    currency: {
+        type: String,
+        required: true,
+    },
+    is_sale: {
+        type: Boolean,
+        required: true,
+    },
+    is_rent: {
+        type: Boolean,
+        required: true,
+    },
+    installment: {
+        type: Boolean,
+        required: true,
+    },
+    rent_type: {
+        type: String,
+        required: true,
+      },
+    rent_dure: {
+        type: String,
+        required: true,
+      },
+    full_Address: {
+        type: String,
+        required: true,
+      },
+    description: {
+        type: String,
+        required: true,
+      },
+    
+    Owner_phoneNumber: {
+          type: String,
+          required: true,
+      },
+     
+})
 
 //create the model 
 
-
+const Real_Estate = mongoose.model('Real_Estate' , Real_Estate)
 
 //export the model
+
+module.exports = Real_Estate
+
+
--- models/User.js
+++ models/User.js
@@ -1,11 +1,55 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique - validator')
 
 
 //create the schema 
+const User = mongoose.Schema({
+    name: {
+        type: String,
+        lowercase: true,
+        required: [true, "can't be blank"],
+        match: [/^[a-zA-Z0-9]+$/, 'is invalid name '],
+        index: true,
+    },
+    email: {
+        type: String,
+        lowercase: true,
+        unique: true,
+        required: [true, "can't be blank"],
+        match:[/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,'is invalid Email'],
+        index: true,
+
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    phoneNumber: {
+        type: String
+    },
+    userType: {
+        isAdmin: {
+            type: Boolean
+        },
+        isOffice: {
+            type: Boolean
+        },
+        isUser: {
+            type: Boolean
+        }
+    },
+    id_real_estate: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Real_Estate'
+    }]
+})
 
 
 //create the model 
 
-
+const User = mongoose.model('User', User)
 
 //export the model
+
+module.exports = User
+
